a9c179d911f47bd03cef9c378b078a38ff7e0e6a
MAHMOUDELSAYED7 <mahmoudelsayed.dev@gmail.com>
2025-10-18 02:30:18 +0300

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and Windows 10/11 compatibility.
    - Implemented utility functions for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Modern Toast Example',
+      title: 'ModernToast Example',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
